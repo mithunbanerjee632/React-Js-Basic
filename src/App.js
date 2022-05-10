93315e873c5a64b33bfebf7e2207875ed8cf0c53
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-05-10 21:44:22 +0600

    Using Bootstrap in React

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './mycomponents/Hello.js';
 import Welcome from './mycomponents/Welcome.js';
+import Myfun from './mycomponents/Myfun';
 
 function App() {
   return (
-    <div className="App">
-        <Hello age="27" name="Mithun Banerjee"></Hello>    
-        
-
-        <Welcome name="i am learning React"></Welcome>   
-              
-    </div>
+       <Myfun></Myfun>
   );
 }
 
---
+++ src/mycomponents/Myfun.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+function Myfun(){
+  return(
+    <button className="btn btn-primary m-10">My Bootstrap</button>
+  )
+}
+export default Myfun;
