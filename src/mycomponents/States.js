50d57b358556775f2e513fbeb98bcb0dd79cce6d
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-05-11 12:02:21 +0600

    Nested State and Array in React

--- src/mycomponents/States.js
+++ src/mycomponents/States.js
@@ -5,9 +5,14 @@ class States extends Component{
       super()
       this.state={
         name:'Mithun Banerjee',
-        age:'27',
+        age:["22","24","25","27"],
         height:'5 feet 6 inch',
-        weight:'65kg'
+        weight:{
+          class7:["40 kg","35kg","38kg"],
+          class10:"50 kg",
+          HSC:"54 kg",
+          BSC:"64 kg"
+        }
       }
 
     }
@@ -17,9 +22,9 @@ class States extends Component{
   render(){
     return <div>
       <h1>Name: {this.state.name}</h1>
-      <h1>Age: {this.state.age}</h1>
+      <h1>Age: {this.state.age[2]}</h1>
       <h1>Height: {this.state.height}</h1>
-      <h1>Weight: {this.state.weight}</h1>
+      <h1>Weight: {this.state.weight.class7[2]}</h1>
     </div>
   }
 }
