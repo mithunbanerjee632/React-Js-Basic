a944d4720571ba4eb4ca3c6334f2720bd4029b0c
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-05-21 21:40:47 +0600

    advance route not found page using react router dom v5

--- src/AdvanceRouting/Myroute.js
+++ src/AdvanceRouting/Myroute.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
-import {Route,Routes} from "react-router-dom";
+import {Route, Routes, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
+import Notfound from "./pages/notfound";
 
 
 class Myroute extends Component {
     render() {
         return (
             <div>
+                <Switch>
                 <Routes>
                 <Route exact path="/" element={<Home/>}/>
-                <Route  path="/about" element={<About/>}/>
-                <Route  path="/contact" element={<Contact/>}/>
-                <Route  path="/portfolio" element={<Portfolio/>}/>
+                <Route exact path="/about" element={<About/>}/>
+                <Route exact path="/contact" element={<Contact/>}/>
+                <Route exact path="/portfolio" element={<Portfolio/>}/>
+                <Route   element={<Notfound/>}/>
                 </Routes>
+                </Switch>
             </div>
         );
     }
