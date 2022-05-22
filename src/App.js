d1ea3dea135bfc8fc8e3f75c56504fdd68d29cbc
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-05-23 01:34:43 +0600

    Passing Data from child to parent

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Myroute from "./AdvanceRouting/Myroute";
 import Mynavigation from "./AdvanceRouting/Mynavigation";
 import State from "./Hook/State";
 import Registration from "./Form/Registration";
+import Child from "./StateLifting/Child";
 function App() {
- 
+   const data = "i am from parent app"
+
+     const handleChildData=(childData)=>{
+       console.log(childData);
+     }
+
     return (
       <div className="App">
 
-          <Registration></Registration>
+         <Child data={data} onChildData={handleChildData}/>
 
           {/*<BrowserRouter>
               <Mynavigation/>
---
+++ src/StateLifting/Child.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Child = (props) => {
+
+    const data = "I am from child component app";
+    props.onChildData(data);
+    return (
+
+        <div>
+            <p>I am Child Component</p>
+            <p>{props.data}</p>
+        </div>
+    );
+};
+
+export default Child;
