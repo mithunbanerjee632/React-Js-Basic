f9f5a7d9cc387c93feee68f65b36412e896ec33a
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-05-24 19:13:27 +0600

    Fetching Data with useEffect Hooks and Error Handling

--- src/Hook/UseEffect/FetchData.js
+++ src/Hook/UseEffect/FetchData.js
@@ -1,25 +1,48 @@
 import React, {useEffect, useState} from 'react';
 
+const loadMessage = <p>Data is Loading</p>
+const errMessage = <p>Error</p>
+
 const FetchData = () => {
     const [todos,setTodos]=useState(null);
+    const [isLoading,setIsLoading] = useState(true)
+    const [error,setError]=useState(null)
+
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/todos")
             .then((response)=>{
-                return response.json();
+                if(!response.ok){
+                    throw Error("Fetching is Not Successfull!");
+                }else{
+                    return response.json();
+                }
+
             })
             .then((data)=>{
                 setTodos(data)
-                console.log(todos);
+                setIsLoading(false);
+            })
+            .catch((error)=>{
+                setError(error.message);
+                setIsLoading(false);
             })
 
     },[])
+
+    const todoElement =  todos &&
+        todos.map((todo)=>{
+            return <p key={todo.id}>{todo.title}</p>
+        })
+
+
+
+
     return (
         <div>
             <h1>Todos</h1>
-            {todos &&
-                todos.map((todo)=>{
-               return <p key={todo.id}>{todo.title}</p>
-            })}
+            {isLoading && loadMessage}
+            {error && errMessage}
+            {todoElement}
         </div>
     );
 };
